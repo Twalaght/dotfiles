fed1a18574431767512ebe5ecf672f6b274dda2d
Twalaght <jonathanchapman6799@gmail.com>
2020-07-21 23:50:53 +0930

    Added first of preexisting scripts

---
+++ .scripts/e6pd.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import argparse
+import base64
+import configparser
+import time
+import urllib.request
+from pathlib import Path
+from requests import get
+
+# Read the arguments given to the script
+parser = argparse.ArgumentParser(description="Download a pool of images from e621")
+parser.add_argument("pool_id", metavar="pool_ID", type=str, help="Pool of images to download (Ex: 11686)")
+parser.add_argument("name", metavar="name", type=str, help="Name template to use when saving the images")
+args = parser.parse_args()
+
+# Read the config file from a specified location
+config = configparser.ConfigParser()
+config.read(Path.home() / ".config" / "auths.txt")
+
+# Set all relevant configuration variables
+user_agent = config.get("e621", "user_agent")
+user_name = config.get("e621", "user_name")
+API_key = config.get("e621", "API_key")
+
+# Calculate the basic auth b64 credential and set the required request header
+user_auth = (user_name + ":" + API_key).encode("ascii")
+b64_auth = "Basic " + base64.b64encode(user_auth).decode("ascii")
+headers = {"User-Agent": user_agent, "Authorization": b64_auth}
+
+# Get all the post IDs belonging to the pool
+pages = get("https://e621.net/pools/" + args.pool_id + ".json", headers=headers).json()["post_ids"]
+time.sleep(1.5)
+
+# Create the required folder with the given name
+Path(args.name).mkdir(parents=True, exist_ok=True)
+
+# Download each page in the pool
+for page in range(len(pages)):
+    # Print a status message for each page
+    print("Downloading page " + str(page + 1) + " of " + str(len(pages)) + "...")
+    
+    # Get the source for each individual image
+    image = get("https://e621.net/posts/" + str(pages[page]) + ".json", headers=headers).json()["post"]["file"]["url"]
+    time.sleep(1.5)
+
+    # Generate the file path for each image to be saved to
+    img_path = Path.cwd() / args.name / (args.name + " " + str(page + 1).zfill(len(str(len(pages)))) + Path(image).suffix)
+
+    # Write the image to disk
+    urllib.request.urlretrieve(image, img_path) 
+    time.sleep(1.5)
+
+# Print a final status when all downloads are finished
+print("Finished downloading " + args.name)
---
+++ .scripts/flac2mp3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Determine which files are present in FLAC folder, but not in MP3 folder
+# Ignores album art, and sanitizes output to be only file names
+flac_list=$(find $MUSIC_PATH/FLAC -type f -printf "%f\n" | sed -e '/^AlbumArt/d' -e 's/\.[^.]*$//')
+mp3_list=$(find $MUSIC_PATH/MP3 -type f -printf "%f\n" | sed -e '/^AlbumArt/d' -e 's/\.[^.]*$//')
+
+# Take the difference of each file list
+diff_out=$(diff <(echo "$flac_list") <(echo "$mp3_list") | grep "<" | sed 's/< //')
+
+# If no changes need to be made, exit the script
+if [ -z "$diff_out" ]; then
+	echo -e "\e[32mNothing to do!\e[39m"
+	exit 0
+fi
+
+# Create MP3 copies of any files that do not already have one
+while IFS= read -r title; do
+	# Store the input and output paths for the given song 
+	flac_path="$MUSIC_PATH/FLAC/$title.flac"
+	mp3_path="$MUSIC_PATH/MP3/$title.mp3"
+
+	# Print a status message
+	printf "%s" "Converting $title... "
+
+	# Convert each FLAC song to an MP3 copy
+	if ffmpeg -i "$flac_path" -ab 320k -map_metadata 0 -id3v2_version 3 -nostdin -loglevel 0 "$mp3_path"; then 
+		# Print a green "done" on success
+		echo -e "\e[32mdone\e[39m"
+	else
+		# Print a red "ERROR" on failure
+		echo -e "\e[31mERROR\e[39m"
+	fi
+done <<< "$diff_out"
