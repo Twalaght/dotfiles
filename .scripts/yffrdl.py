4c97045236b905ceb60a67b5412e86456d209735
Twalaght <52785900+Twalaght@users.noreply.github.com>
2021-07-27 22:32:02 +0930

    Reworked yffr downloader

--- .scripts/yffrdl.py
+++ .scripts/yffrdl.py
@@ -8,50 +8,32 @@ from urllib.parse import unquote_plus
 # Read the arguments given to the script
 parser = argparse.ArgumentParser(description="Download images sets from yiffer.xyz")
 parser.add_argument("target", metavar="url", type=str, help="target url to download")
+parser.add_argument("length", metavar="length", type=int, help="set the number of images to download")
 parser.add_argument("-t", "--title", type=str, help="set the title of the downloaded images")
 args = parser.parse_args()
 
-# Set the first image, the URL encoded name, and the regular name
-image_id = 1
-name = args.target.split("/")[-1]
-comic_name = unquote_plus(name)
+# Set the plain text name of the comic, file name, and the amount to pad numbers
+comic_name = unquote_plus(args.target.split("/")[-1])
+file_name = args.title if args.title else comic_name
+padding = len(str(args.length))
 
-# Create the required folder with the title if provided, or the comic name
-if args.title:
-    print(f"[Starting download for {args.title}]")
-    Path(args.title).mkdir(parents=True, exist_ok=True)
-else:
-    print(f"[Starting download for {comic_name}]")
-    Path(comic_name).mkdir(parents=True, exist_ok=True)
+# Print a status message, and create the required folder with an appropriate name
+print(f"[Downloading {file_name} with {args.length} images]")
+Path(file_name).mkdir(parents=True, exist_ok=True)
 
 # Iterate until every image has been requested
-while True:
-    name = args.target.split("/")[-1]
-    page = "{0:02d}.jpg".format(image_id)
-    source = f"{args.target}/../comics/{name}/{page}"
-
+for image_id in range(1, args.length + 1):
     # Send the request for the target url, and break if code 200 is not received
-    image_data = get(source)
-    if image_data.status_code != 200:
-        break
+    image_data = get(f"{args.target.replace('yiffer.xyz', 'static.yiffer.xyz/comics')}/{image_id:03}.jpg")
+    if image_data.status_code != 200: break
 
     # Print a status message for each page
-    print(f"Downloading page {image_id}...")
+    print(f"[Downloading image {image_id} of {args.length}]", end = "\r")
 
     # Set the appropriate image path to download to, then download it
-    if args.title:
-        image_path = Path.cwd() / args.title / f"{args.title} {page}"
-    else:
-        image_path = Path.cwd() / comic_name / f"{comic_name} {page}"
-
-    with open(image_path, "wb") as image:
-        image.write(image_data.content)
-
-    # Increase the page number and keep downloading
-    image_id = image_id + 1
+    file_name = args.title if args.title else comic_name
+    image_path = Path.cwd() / file_name / f"{file_name} {image_id:0{padding}}.jpg"
+    with open(image_path, "wb") as image: image.write(image_data.content)
 
 # Print a final status when all downloads are finished
-if args.title:
-    print(f"[Finished downloading {args.title}]")
-else:
-    print(f"[Finished downloading {comic_name}]")
+print(f"\n[Finished {file_name}]")
