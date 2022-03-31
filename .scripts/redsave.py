0886d02f17efe5dd6080e9903d045a7b156a8f74
Twalaght <52785900+Twalaght@users.noreply.github.com>
2022-03-31 13:18:07 +1030

    Fixed issue on files with no audio

--- .scripts/redsave.py
+++ .scripts/redsave.py
@@ -1,37 +1,50 @@
 #!/usr/bin/python3
 
 import argparse
-import os
-import re
-import urllib.request
+from os import remove
 from pathlib import Path
+from re import sub
 from requests import get
+from shutil import copyfile
+from subprocess import call
+from urllib.request import urlretrieve
 
 # Take the reddit video url as an argument
 parser = argparse.ArgumentParser(description="Download a video hosted on reddit")
 parser.add_argument("url", metavar="url", type=str, help="Reddit URL to download from")
 args = parser.parse_args()
 
-# Send a get request and find the video source url
-json = get(f"{args.url}.json", headers={"User-Agent": "Wirewolf"}).json()
-title = json[0]["data"]["children"][0]["data"]["title"]
-json = json[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
+# Set the header and send a get request and find the video source url
+headers = {"User-Agent": "Redsave"}
+json = get(f"{args.url}.json", headers = headers).json()
+url = json[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
 
 # Create a downloadable link for video and audio
-video = re.sub(r"\?.*$", "", json)
-audio = re.sub(r"[0-9]+.mp4\?.*$", "audio.mp4", json)
+video = sub(r"\?.*$", "", url)
+audio = sub(r"[0-9]+.mp4\?.*$", "audio.mp4", url)
 
-# Sanitize the title to be valid for a file
-title = title.replace(" ", "_")
-title = re.sub(r"\W+", "", title)
+# Set the title of the video and limit the length
+title = json[0]["data"]["children"][0]["data"]["title"].replace(" ", "_")
+title = sub(r"\W+", "", title)
+title = f"{title[:100]}.mp4"
 
-# Download the audio and video
-urllib.request.urlretrieve(video, Path.cwd() / "video.mp4")
-urllib.request.urlretrieve(audio, Path.cwd() / "audio.mp4")
+# Check if the target video has audio
+has_audio = get(audio, headers = headers).status_code == 200
 
-# Combine the two files in ffmpeg
-os.system(f"ffmpeg -i {Path.cwd() / 'video.mp4'} -i {Path.cwd() / 'audio.mp4'} -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 {title}.mp4 2> /dev/null")
+# Download the video component
+urlretrieve(video, Path("/tmp/video.mp4"))
 
-# # Delete the two temp files
-os.remove(Path.cwd() / "video.mp4")
-os.remove(Path.cwd() / "audio.mp4")
+# Check if the target has audio
+if get(audio, headers = headers).status_code == 200:
+	# If the file has audio, download it and merge with ffmpeg
+	urlretrieve(audio, Path("/tmp/audio.mp4"))
+	command = f"ffmpeg -i {Path('/tmp/video.mp4')} -i {Path('/tmp/audio.mp4')} -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 {Path.cwd() / title} > /dev/null 2>&1"
+	call(command, shell = True)
+
+else:
+	# If no audio is present, just copy the video
+	copyfile(Path("/tmp/video.mp4"), Path.cwd() / title)
+
+# Remove the temporary files
+remove(Path("/tmp/video.mp4"))
+if has_audio: remove(Path("/tmp/audio.mp4"))
