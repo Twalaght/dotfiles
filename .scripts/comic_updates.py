0908ad024942402e7dc75a558c629e1fa6ade384
Twalaght <jonathanchapman6799@gmail.com>
2022-07-01 23:21:37 +0930

    Revamp to pool downloader and comic watcher

--- .scripts/comic_updates.py
+++ .scripts/comic_updates.py
@@ -1,52 +1,96 @@
 #!/usr/bin/python3
 
-from base64 import b64encode
-from configparser import ConfigParser
-from json import load
+from e6pd import gen_headers, e6pd
 from os import getenv
 from pathlib import Path
 from requests import get
+from sqlite3 import connect
 from time import sleep
+
+# Read in a watchlist from a database
+def read_db(watchlist_path):
+	# Ensure the watchlist database exists
+	wl = Path(watchlist_path) / "watchlist.db"
+	if not wl.exists():
+		print(f"Error - watchlist.db does not exist at {watchlist_path}")
+		exit(1)
+
+	# Connect to the database and create a cursor
+	db = connect(wl)
+	cursor = db.cursor()
+
+	# Write a query and execute it with cursor
+	query = "SELECT * FROM e621_comics"
+	cursor.execute(query)
+
+	# Fetch and output result
+	result = cursor.fetchall()
+	cursor.close()
+	return result
 
 # Set the base directory from the environment variable
 base_dir = getenv("COMIC_PATH")
-if base_dir:
-	base_dir = Path(base_dir)
-else:
-	print(f"No comic path environment variable, please set it")
-	exit()
+if base_dir is None:
+	print(f"Error - COMIC_PATH environment variable is not set")
+	exit(1)
 
-# Set up the config parser and read in the auths file
-config = ConfigParser()
-auths = Path.home() / ".config" / "auths.txt"
-if auths.exists():
-	config.read(auths)
-else:
-	print(f"Authentication not found at {auths}, please create it")
-	exit()
-
-# Calculate the b64_auth credential and set the request header
-b64_auth = b64encode(f"{config.get('e621', 'user_name')}:{config.get('e621', 'API_key')}".encode("ascii")).decode("ascii")
-headers = {"User-Agent": config.get("e621", "user_agent"), "Authorization": f"Basic {b64_auth}"}
-
-# Read in the watchlist file
-with open(base_dir / "watchlist.json") as f: watchlist = load(f)
+# Set the base comic directory, generate headers, and read the database
+base_dir = Path(base_dir)
+headers = gen_headers()
+comics = read_db(base_dir)
 
 # For each comic in the watchlist check its local data against the server
-for comic in watchlist["comics"]:
-	# Count how many local files we have
-	local_num = comic["offset"]
-	for page in Path(base_dir / comic["folder"]).iterdir():
+for comic in comics:
+	# Count how many local files we have, with an offset included
+	local_num = -comic[3]
+
+	# Ensure the target comic folder exists
+	if not Path(base_dir / comic[2]).exists():
+		print(f"WARNING - Folder {comic[2]} does not exist")
+		continue
+
+	# Count the number of files present locally
+	for page in Path(base_dir / comic[2]).iterdir():
 		if page.is_file(): local_num += 1
 
-	# Query the server for total number of files, and get the name too
-	server_num = get(f"https://e621.net/pools/{comic['id']}.json", headers = headers).json()["post_count"]
+	# Query e621 to find how many pages are listed
+	server_num = get(f"https://e621.net/pools/{comic[0]}.json", headers = headers).json()["post_count"]
 
+	# Output a status message depending on the status of the comic
 	if local_num < server_num:
-		print(f"* {comic['name']} has {server_num - local_num} new pages")
+		print(f"* {comic[1]} has {server_num - local_num} new pages")
+
+		# If the update flag is set, update the comic
+		if comic[4] == 1:
+			# Set the offsets corresponding to deleted posts or combined pools
+			n_offset = comic[3] if comic[3] > 0 else None
+			if comic[3] < 0: local_num += comic[3]
+
+			# Set the download folder and update the required pages
+			dl_folder = base_dir / comic[2]
+			e6pd(comic[0], comic[1], local_num, n_offset, None, dl_folder, True, headers)
+
 	elif local_num > server_num:
-		print(f"\033[34m{comic['name']} is ahead of e6 by {local_num - server_num} pages\033[0m")
+		print(f"\033[34m{comic[1]} is ahead of e6 by {local_num - server_num} pages\033[0m")
 	else:
-		print(f"\033[32m{comic['name']} is up to date\033[0m")
+		print(f"\033[32m{comic[1]} is up to date\033[0m")
 
 	sleep(1)
+
+
+
+# # Connect to DB and create a cursor
+# db = sqlite3.connect("watchlist.db")
+# cursor = db.cursor()
+
+# for comic in json_watchlist["comics"]:
+# 	id, folder, name, offset = comic.values()
+# 	name = name.replace("'","''");
+# 	folder = folder.replace("'","''");
+# 	query = f"INSERT INTO e621_comics VALUES ({id}, '{name}', '{folder}', {offset})"
+# 	cursor.execute(query)
+
+# # Close the cursor and commit
+# cursor.close()
+# db.commit()
+# exit()
