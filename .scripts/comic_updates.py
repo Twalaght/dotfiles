7069c64c4ab33bb37590a5e4401af4d83c0b3254
Twalaght <jonathanchapman6799@gmail.com>
2023-03-19 23:11:06 +1030

    Comic updater adapted to use json

--- .scripts/comic_updates.py
+++ .scripts/comic_updates.py
@@ -1,175 +1,103 @@
 #!/usr/bin/python3
 
-from bs4 import BeautifulSoup
+from argparse import ArgumentParser
 from e6pd import gen_headers, e6pd, parallel_download
-from multiprocessing import Pool, cpu_count
 from os import getenv
 from pathlib import Path
 from requests import get
-from sqlite3 import connect
 from time import sleep
+import json
 
-# Read in a watchlist from a database
-def read_db(watchlist, table):
-	# Ensure the watchlist database exists
-	if not watchlist.exists():
-		print(f"Error - watchlist.db does not exist at {watchlist_path}")
+# Read and verify the base directory and database file
+def setup():
+	# Read the base directory from the environment variable
+	base_dir = getenv("COMIC_PATH")
+	if base_dir is None:
+		print("Error - COMIC_PATH environment variable is not set")
 		exit(1)
 
-	# Connect to the database and execute a query
-	query = f"SELECT * FROM {table}"
-	cursor = connect(watchlist).cursor()
-	cursor.execute(query)
+	# Set the base directory and read in the database file
+	base_dir = Path(base_dir)
+	comic_database = base_dir / "comics.json"
+	if not comic_database.exists():
+		print(f"Error - {comic_database} does not exist")
+		exit(1)
 
-	# Fetch and output result
-	result = cursor.fetchall()
-	cursor.close()
-	return result
+	with open(base_dir / "comics.json") as f:
+		comics = json.load(f)
 
-# Update a database entry with the newest page link
-def update_page(watchlist, page, name):
-	# Connect to the database and execute a query
-	query = f"UPDATE webcomics SET url = '{page}' WHERE name = '{name}'"
-	db = connect(watchlist)
-	cursor = db.cursor()
-	cursor.execute(query)
+	# Sort the ongoing entries and return the data
+	comics["ongoing"].sort(key = lambda x: x["name"])
+	return base_dir, comics
 
-	# Commit to the database and close the connection
-	db.commit()
-	cursor.close()
+# Download new comic pages hosted on e621
+def download_updates(base_dir, comics):
+	headers = gen_headers()
+	for comic in comics:
+		# Ensure the target comic folder exists
+		if not (base_dir / comic["name"]).exists():
+			print(f"WARNING - Folder {base_dir / comic['name']} does not exist")
+			continue
 
-# Collect images from starting point of a webcomic
-def chain(base_dir, comic):
-	# Count the number of files present locally
-	local_num = 1
-	for page in Path(base_dir / comic[2]).iterdir():
-		if page.is_file(): local_num += 1
+		# Count how many local files we have, with an offset included
+		local_num = -comic["offset"]
+		for page in (base_dir / comic["name"]).iterdir():
+			if page.is_file(): local_num += 1
 
-	# Iterate through pages to collect the links to new images
-	page = comic[0]
-	images = []
-	while True:
-		# Extract the image URL and print a status message
-		soup = BeautifulSoup(get(page).text, "html.parser")
-		images.append(parser[comic[3]].image(soup))
-		print(f"[Collected {len(images)} pages]", end = "\r")
+		# Query e621 to find how many pages are listed
+		server_num = get(f"https://e621.net/pools/{comic['id']}.json", headers = headers).json()["post_count"]
 
-		# If the final page is reached, break out of the loop
-		old_page = page
-		page = parser[comic[3]].page(soup)
-		if not page or page == old_page:
-			page = old_page
-			print()
-			break
+		# Download the new pages if the server has more than local
+		if local_num < server_num:
+			print(f"* {comic['name']} has {server_num - local_num} new pages")
 
-	# Prevent downloading the most recent page twice
-	if local_num != 1: images = images[1:]
+			# If the update flag is set, update the comic
+			if comic["update"]:
+				# Set the offsets corresponding to deleted posts or combined pools
+				n_offset = comic["offset"] if comic["offset"] > 0 else None
+				if comic["offset"] < 0: local_num += comic["offset"]
 
-	# Generate download arguments from the fetched images
-	args = []
-	for i in range(len(images)):
-		ext = images[i].split(".")[-1]
-		name = f"{comic[1]} {local_num + i:04}.{ext}"
-		path = base_dir / comic[2] / name
-		args.append((images[i], path, i + 1, len(images)))
+				# Set the download folder and download the required pages
+				dl_folder = base_dir / comic["name"]
+				e6pd(comic["id"], comic["name"], local_num, n_offset, None, dl_folder, True, headers)
 
-	return page, args
+		# Report if the comic is ahead or matching the server
+		elif local_num > server_num:
+			print(f"\033[34m{comic['name']} is ahead of e6 by {local_num - server_num} pages\033[0m")
+		else:
+			print(f"\033[32m{comic['name']} is up to date\033[0m")
 
-# Define image and page behavior for each webcomic
-class pix_and_hen:
-	def image(soup):
-		image = soup.find("div", {"class": "col-lg-8 container-fluid text-center"})
-		return image.figure.img["src"].split("?")[0]
-	def page(soup):
-		next_page = soup.find("a", {"rel": "next"})
-		return next_page["href"] if next_page else None
-class housepets:
-	def image(soup):
-		image = soup.find("div", {"id": "comic"})
-		return image.find("img")["src"]
-	def page(soup):
-		next_page = soup.find("a", {"class": "navi comic-nav-next navi-next"})
-		return next_page["href"] if next_page else None
+		sleep(1)
 
-parser = {"pix_and_hen": pix_and_hen, "housepets": housepets}
+# Verify local files against the database
+def check(base_dir, comics, regenerate):
+	# Iterate over local files given by the paths in the database
+	for path in comics["paths"]:
+		for comic in (base_dir / path).iterdir():
+			tmp = comic.name.split(" - ")
 
-# Set the base directory from the environment variable
-base_dir = getenv("COMIC_PATH")
-if base_dir is None:
-	print(f"Error - COMIC_PATH environment variable is not set")
-	exit(1)
+			# Report new items and add them to the database
+			if tmp[1] not in comics["finished"]:
+				print(f"{tmp[1]} - found locally but not in the database")
+				comics["finished"][tmp[1]] = {"artist": tmp[0], "tags": ""}
 
-# Set the base comic directory, generate headers, and read the database
-base_dir = Path(base_dir)
-headers = gen_headers()
-e6_comics = read_db(base_dir / "watchlist.db", "e621_comics")
-web_comics = read_db(base_dir / "watchlist.db", "webcomics")
+	# Write out a new database file if required
+	if regenerate:
+		new_database = base_dir / "comics_NEW.json"
+		print(f"Writing new database file to {new_database}")
+		with open(new_database, "w") as f:
+			json.dump(comics, f, indent = 4)
 
-# Sort the fetched comics entries by name
-e6_comics.sort(key = lambda x: x[1])
-web_comics.sort(key = lambda x: x[1])
+if __name__ == "__main__":
+	# Read the arguments given to the script
+	parser = ArgumentParser(description="Update local comics based on a database file")
+	parser.add_argument("-c", "--check", action="store_true", help="verify finished comics folders against the database")
+	parser.add_argument("-r", "--regenerate", action="store_true", help="create a new database file based on the existing")
+	args = parser.parse_args()
 
-# Process each comic hosted on e621
-for comic in e6_comics:
-	# Count how many local files we have, with an offset included
-	local_num = -comic[3]
-
-	# Ensure the target comic folder exists
-	if not Path(base_dir / comic[2]).exists():
-		print(f"WARNING - Folder {comic[2]} does not exist")
-		continue
-
-	# Count the number of files present locally
-	for page in Path(base_dir / comic[2]).iterdir():
-		if page.is_file(): local_num += 1
-
-	# Query e621 to find how many pages are listed
-	server_num = get(f"https://e621.net/pools/{comic[0]}.json", headers = headers).json()["post_count"]
-
-	# Output a status message depending on the status of the comic
-	if local_num < server_num:
-		print(f"* {comic[1]} has {server_num - local_num} new pages")
-
-		# If the update flag is set, update the comic
-		if comic[4] == 1:
-			# Set the offsets corresponding to deleted posts or combined pools
-			n_offset = comic[3] if comic[3] > 0 else None
-			if comic[3] < 0: local_num += comic[3]
-
-			# Set the download folder and update the required pages
-			dl_folder = base_dir / comic[2]
-			e6pd(comic[0], comic[1], local_num, n_offset, None, dl_folder, True, headers)
-
-	elif local_num > server_num:
-		print(f"\033[34m{comic[1]} is ahead of e6 by {local_num - server_num} pages\033[0m")
+	# Perform setup and either check the database or download updates
+	base_dir, comics = setup()
+	if args.check or args.regenerate:
+		check(base_dir, comics, args.regenerate)
 	else:
-		print(f"\033[32m{comic[1]} is up to date\033[0m")
-
-	sleep(1)
-
-# Process each webcomic
-for comic in web_comics:
-	# Ensure the target comic folder exists
-	if not Path(base_dir / comic[2]).exists():
-		print(f"WARNING - Folder {comic[2]} does not exist")
-		continue
-
-	# Get the link to the next page
-	soup = BeautifulSoup(get(comic[0]).text, "html.parser")
-	page = parser[comic[3]].page(soup)
-
-	# Continue if the latest page has not changed
-	if not page or page == comic[0]:
-		print(f"\033[32m{comic[1]} is up to date\033[0m")
-		continue
-
-	# Print a status message and collect the new pages
-	print(f"* {comic[1]} has new pages")
-	new_page, args = chain(base_dir, comic)
-
-	# Create a pool of threads, and use them to download in parallel
-	Pool(cpu_count()).starmap(parallel_download, args, chunksize = 1)
-
-	# Update the database entry and print a closing status message
-	update_page(base_dir / "watchlist.db", new_page, comic[1])
-	print(f"\n[Finished {comic[1]}]")
+		download_updates(base_dir, comics["ongoing"])
